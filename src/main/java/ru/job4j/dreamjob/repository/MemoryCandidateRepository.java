7cf072ee9f54f9e083b46e6c7df88e08684e0524
AGretskikh <gretskih@mail.ru>
2023-10-10 12:00:32 +0300

    HTML form. Создание страницы кандидата

--- src/main/java/ru/job4j/dreamjob/repository/MemoryCandidateRepository.java
+++ src/main/java/ru/job4j/dreamjob/repository/MemoryCandidateRepository.java
@@ -24,11 +24,11 @@ public class MemoryCandidateRepository implements CandidateRepository {
     }
 
     private MemoryCandidateRepository() {
-        save(new Candidate(0, "Java Developer", "Java 17; Spring; PostgreSQL; HTML 5",
+        save(new Candidate(0, "Антонов Илья", "Java 17; Spring; PostgreSQL; HTML 5",
                 LocalDateTime.of(2023, Month.OCTOBER, 1, 8, 30)));
-        save(new Candidate(0, "Junior Java Developer", "Java 8; Spring MVC; Junit 5; CSS",
+        save(new Candidate(0, "Клушин Федор", "Java 8; Spring MVC; Junit 5; CSS",
                 LocalDateTime.of(2023, Month.OCTOBER, 4, 9, 30)));
-        save(new Candidate(0, "Middle Java Developer", "Java 18; Spring Framework; Multithreading; J2E",
+        save(new Candidate(0, "Путин Петр", "Java 18; Spring Framework; Multithreading; J2E",
                 LocalDateTime.of(2023, Month.OCTOBER, 5, 8, 0)));
     }
 
